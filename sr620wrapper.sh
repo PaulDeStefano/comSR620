7a85bd9232532ea46370c21fd5204590c0ebb3e8
Paul R. DeStefano <paul (dot) destefano (ta)willamettealumni (dot) c o m>
2014-05-04 22:36:35 -0700

    Added sr620wrapper script

---
+++ sr620wrapper.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+arguments=$@
+sr620path="/DATA/LSU_TIC/"
+defaultDev=/dev/ttyUSB0 # LSU-TIC is on ttyUSB0 on gpsptnu1 & sukrnh5/skgpspt
+
+PID=$(pgrep sr620)
+
+for process in $PID; do
+#	echo "$process"
+	procargs=$(ps -o args -p $process)
+	argarray=( $procargs )
+	n=$((${#argarray[@]}-1))
+	for (( i=0; i<=n; i++ ))
+	do
+#		echo "${argarray[i]}"
+		if [ ${argarray[i]} == "-d" ] || [ ${argarray[i]} == "--device" ] && [ ${argarray[i+1]} == "${defaultDev}" ]
+		then
+                    isRunning="true"
+		fi
+	done
+done
+
+if [ $1 == "bootmode" ]
+then
+    if [ "${isRunning}" == "true" ]
+    then
+	echo "sr620 is already running with the device ${defaultDev}"
+	exit 1
+    else
+	cd $sr620path
+	#screen -d -m -S sr620screen ./sr620 -d "${defaultDev}" -l NU1 -a OT -b PT_Sept -t LSUTIC --rotate
+	screen -d -m -S sr620screen bash -c "./sr620 -d ${defaultDev} -l Kenkyuto -a OT -b PT_Sept -t LSUTIC --rotate 2>&1 | tee -a sr620.log"
+	exit $?
+    fi
+fi
+
+if [ $1 == "checkmode" ]
+then
+    if [ "${isRunning}" == "true" ]
+    then
+	echo "sr620 is running with the device ${defaultDev}"
+	exit 0
+    else
+	exit 1
+   fi
+fi
+
+if [ "${isRunning}" == "true" ]
+then
+    userargs=( $arguments )
+    n=$((${#userargs[@]}-1))
+    for (( i=0; i<=n; i++ ))
+    do
+#		echo "${userargs[i]}"
+	if [ ${userargs[i]} == "-d" ] || [ ${userargs[i]} == "--device" ] && [ ${userargs[i+1]} == "${defaultDev}" ]
+	then
+	    echo "sr620 is already running with device ${defaultDev}"
+	    exit 1
+	fi
+    done
+fi
+./sr620 $arguments | tee -a "sr620.log"
+#./sr620 $arguments
+exit $?
